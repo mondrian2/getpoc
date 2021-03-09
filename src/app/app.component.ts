f8197f652ea20035e1a50ae287f342359cdaca5f
daniel <deecomunicacao@gmail.com>
2021-03-09 14:08:13 -0300

    service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ExerciciosService} from './services/exercicios.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'getpoc';
+  constructor(private srv: ExerciciosService) {
+  }
+  ngOnInit(): void {
+    this.srv.get().subscribe(r => console.log(r));
+  }
 }
